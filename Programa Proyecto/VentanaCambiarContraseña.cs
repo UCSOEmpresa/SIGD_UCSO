397b48f016a93ff61fefcffab79a96ac02d8ca13
Mauro Aguirre <mugiwaramau@gmail.com>
2022-07-17 00:17:27 -0300

    Nuevos Cambios en el programa

---
+++ Programa Proyecto/VentanaCambiarContraseña.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_Proyecto
+{
+    public partial class VentanaCambiarContraseña : Form
+    {
+        public VentanaCambiarContraseña()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtCorreo.Text != "")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Esta vacío");
+            }
+
+            string agregar = "";
+            string a = txtCorreo.Text;
+
+
+            if (a.Contains("@") && a.Contains(".com"))
+            {
+                agregar += " " + a;
+                MessageBox.Show("Se le ha enviado un mail con instrucciones para restablecer su contraseña");
+                txtCorreo.Clear();
+            }
+            else
+            {
+                MessageBox.Show("El correo introducido es incorrecto");
+                txtCorreo.Clear();
+            }
+        }
+
+        private void pBIcono_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnSN_Click(object sender, EventArgs e)
+        {
+            
+            VentanaSobreNosotros frm = new VentanaSobreNosotros();
+            frm.Show();
+        }
+
+        private void btnSoporte_Click(object sender, EventArgs e)
+        {
+            VentanaSoporte frm = new VentanaSoporte();
+            frm.Show();
+        }
+    }
+}
---
+++ Programa Proyecto/VentanaJugadores.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_Proyecto
+{
+    public partial class VentanaJugadores : Form
+    {
+        public VentanaJugadores()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pBAtras_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (txtBusqueda.Text != "")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Esta vacío");
+            }
+        }
+
+        private void btnFixture_Click(object sender, EventArgs e)
+        {
+            if (fLFixture.Visible == true)
+            {
+                fLFixture.Visible = false;
+            }
+            else
+            {
+                fLFixture.Visible = true;
+            }
+        }
+
+        private void btnTorneos_Click(object sender, EventArgs e)
+        {
+            if (fLTorneos.Visible == true)
+            {
+                fLTorneos.Visible = false;
+            }
+            else
+            {
+                fLTorneos.Visible = true;
+            }
+        }
+
+        private void btnJugadores_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEquipos_Click(object sender, EventArgs e)
+        {
+            VentanaEquipos frm = new VentanaEquipos();
+            frm.Show();
+        }
+
+        private void Clear(object sender, MouseEventArgs e)
+        {
+            txtBusqueda.Clear();
+        }
+
+        private void Error(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Ya te encuentras en la ventana Jugadores");
+        }
+
+        private void VentanaJugadores_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContacto_Click(object sender, EventArgs e)
+        {
+            VentanaContacto frm = new VentanaContacto();
+            frm.Show();
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+        }
+
+        private void lblBasket_Click(object sender, EventArgs e)
+        {
+            VentanaFixtureBasketball frm = new VentanaFixtureBasketball();
+            frm.Show();
+        }
+
+        private void lblHandball_Click(object sender, EventArgs e)
+        {
+            VentanaFixtureHandball frm = new VentanaFixtureHandball();
+            frm.Show();
+        }
+
+        private void lblFutbol2_Click(object sender, EventArgs e)
+        {
+            VentanaTorneosFutbol frm = new VentanaTorneosFutbol();
+            frm.Show();
+        }
+
+        private void lblBasket2_Click(object sender, EventArgs e)
+        {
+            VentanaTorneosBasketball frm = new VentanaTorneosBasketball();
+            frm.Show();
+        }
+
+        private void lblHandball2_Click(object sender, EventArgs e)
+        {
+            VentanaTorneosHandball frm = new VentanaTorneosHandball();
+            frm.Show();
+        }
+
+        private void btnIdioma_Click(object sender, EventArgs e)
+        {
+            if (fLIdioma.Visible == true)
+            {
+                fLIdioma.Visible = false;
+            }
+            else
+            {
+                fLIdioma.Visible = true;
+            }
+        }
+
+        private void lblFutbol_Click(object sender, EventArgs e)
+        {
+            VentanaFixtureFutbol frm = new VentanaFixtureFutbol();
+            frm.Show();
+        }
+
+        private void btnSN_Click(object sender, EventArgs e)
+        {
+            VentanaSobreNosotros frm = new VentanaSobreNosotros();
+            frm.Show();
+        }
+
+        private void Borrar(object sender, MouseEventArgs e)
+        {
+            txtBuscarJu.Clear();
+        }
+
+        private void btnSoporte_Click(object sender, EventArgs e)
+        {
+            VentanaSoporte frm = new VentanaSoporte();
+            frm.Show();
+        }
+
+        private void btnBuscarJu_Click(object sender, EventArgs e)
+        {
+            if (txtBuscarJu.Text != "")
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Esta vacío");
+            }
+        }
+    }
+}
